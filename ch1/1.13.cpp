f56ec60927db760344bb2382295881a5da74bd89
hbj1572 <kimhanbj@naver.com>
2025-01-06 16:52:59 +0900

    Update 1.13.cpp

--- ch1/1.13.cpp
+++ ch1/1.13.cpp
@@ -1,8 +1,33 @@
-#include <iostream> 
-    int main(){
+#include <iostream>//1.9
+int main (){
     int sum = 0;
-    for (int i = 50; i <= 100; i++)
+    for (int i = 50; i <= 100; i++){
         sum += i;
-        std::cout << sum <<std::endl;
-        return 0;
+        std::cout << sum << std::endl;
+    }
+    return 0;
+}
+
+
+#include <iostream>//1.10
+int main (){
+    int n1 = 10;
+    for (int i = n1; i >= 0; i--){
+        std::cout << n1 << std::endl;
+        n1--;
+    }
+    return 0;
+}
+
+
+#include <iostream>//1.11
+int main (){
+    int n1 = 0, n2 = 0, n3;
+    std::cin >> n1 >> n2;
+    for (int i = n2; i >= n1; i--){
+        n3 = n2;
+        --n2;
+        std::cout << n3 << std::endl;
+    }
+    return 0;
 }
